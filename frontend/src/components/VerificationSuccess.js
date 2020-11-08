d25550603d151c34637eb4765ba1504e2be7e4bc
Selvaraj Chinnasamy <selvaraj.chinnasamy@softwaysolutions.com>
2020-11-08 19:57:26 +0530

    vouched implementation and update on verification process

---
+++ frontend/src/components/VerificationSuccess.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import { Spinner } from "react-bootstrap";
+import { BrowserRouter as Router, useLocation, useHistory } from "react-router-dom";
+import config from "../config/front_config";
+import { Container } from "react-bootstrap";
+
+const VerificationSuccess = () => {
+
+    const history = useHistory();
+    const [user, setUser] = useState();
+    const [isLoading, setIsLoading] = useState();
+
+    const { search } = useLocation();
+    const query = new URLSearchParams(search);
+    const token = query.get('token');
+
+    useEffect(() => {
+        getUserWithToken();
+    }, []);
+
+    const getUserWithToken = () => {
+        setIsLoading(true);
+        fetch(config.api.getUser, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                token
+            })
+        }).then(res => res.json()).then(data => {
+            setIsLoading(false);
+            if (data && data['data'] && data['data']['token']) {
+                setUser(data['data']);
+                if (data['data']['id_verification_result'] == "verified") {
+                    if (!(data['data']['verify_result'])) {
+                        if (data['data']['verify_photo']) {
+                            history.push('/userid?token=' + token);
+                        } else {
+                            history.push('/userphoto?token=' + token);
+                        }
+                    }
+                } else {
+                    history.push('/verifyID?token=' + token);
+                }
+            } else {
+                alert('User not found!.');
+            }
+        }).catch(err => {
+            setIsLoading(false);
+        })
+    }
+
+    return (
+        <Router>
+            <Container style={{ textAlign: "center" }}>
+                {isLoading ? <div
+                    style={{
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        textAlign: 'center',
+                        backgroundColor: 'rgba(16, 16, 16, 0.5)',
+                        zIndex: 999
+                    }}>
+                    <Spinner style={{ textAlign: 'center', marginTop: '30%' }} animation="border" />
+                </div> : null}
+                <div>
+                    <ul class="progressbar">
+                        <li class="active">Identity Verification</li>
+                        <li class="active">Photo Verification</li>
+                        <li class="active">ID Verification</li>
+                        <li class="active">Verification completed successfully</li>
+                    </ul>
+                </div>
+                <div style={{ paddingTop: 200 }}>
+                    <h1>Congratulation!, you have completed verification process</h1>
+                </div>
+            </Container>
+        </Router>
+    );
+}
+
+export default VerificationSuccess;
