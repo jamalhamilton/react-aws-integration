dc5badba4db4847d068b1bd618f510ee7fb6fbbd
Selvaraj Chinnasamy <selvaraj.chinnasamy@softwaysolutions.com>
2020-11-12 15:35:50 +0530

    fix

--- frontend/src/components/VerificationBase.js
+++ frontend/src/components/VerificationBase.js
@@ -125,7 +125,18 @@ const VerificationBase = () => {
             });
     }
 
+    const dataURItoBlob = (dataURI) => {
+        var binary = atob(dataURI.split(',')[1]);
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        var array = [];
+        for (var i = 0; i < binary.length; i++) {
+            array.push(binary.charCodeAt(i));
+        }
+        return new Blob([new Uint8Array(array)], { type: mimeString });
+    }
+
     const uploadPhotoID = (imageToUpload) => {
+        imageToUpload = dataURItoBlob(imageToUpload);
         AWS.config.update({
             accessKeyId: config.aws.accessKey,
             secretAccessKey: config.aws.secretKey,
