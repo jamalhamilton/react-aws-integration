fd0a5c9cb7595779a0f0447b19d029c26ee50607
jamal <eugenehamilton843@gmail.com>
2020-11-20 03:16:26 +0800

    update build

--- frontend/build/precache-manifest.5b827dd68abfca2bfb779771c2d51d68.js
+++ frontend/build/precache-manifest.5b827dd68abfca2bfb779771c2d51d68.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5a8e56ea0cff2612c3bb608c2601546c",
+    "revision": "3a4fea26a392ab579e9a9c4137cf294d",
     "url": "/index.html"
   },
   {
-    "revision": "79cb14b6e981d32306c7",
+    "revision": "6c85d8b356d880d13bf2",
     "url": "/static/css/main.faf40f4c.chunk.css"
   },
   {
-    "revision": "bc709b52abd922f750ef",
-    "url": "/static/js/2.38fd2d32.chunk.js"
+    "revision": "85567d64fcf3697dd008",
+    "url": "/static/js/2.70a82e04.chunk.js"
   },
   {
     "revision": "a3550eec1a65acbb2f1a9000b279b5f0",
-    "url": "/static/js/2.38fd2d32.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.70a82e04.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "79cb14b6e981d32306c7",
-    "url": "/static/js/main.28b7f04f.chunk.js"
+    "revision": "6c85d8b356d880d13bf2",
+    "url": "/static/js/main.d6180689.chunk.js"
   },
   {
     "revision": "5b3da2b025d0e5549e0a",
